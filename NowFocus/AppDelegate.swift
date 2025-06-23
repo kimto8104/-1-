066b58b8dacec8e6e1d01b4af8ea5c39b115818b
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2025-06-23 21:21:17 +0900

    Implement Firebase to this Project

--- NowFocus/AppDelegate.swift
+++ NowFocus/AppDelegate.swift
@@ -11,7 +11,8 @@ import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    firebaseConfigure()
+    
     UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
       if granted {
         print("通知の許可が得られました")
@@ -20,6 +21,24 @@ class AppDelegate: NSObject, UIApplicationDelegate {
       }
     }
     return true
+  }
+  
+  private func firebaseConfigure() {
+    #if DEBUG
+    let filePath = Bundle.main.path(forResource: "GoogleService-Info-Debug", ofType: "plist")
+    #else
+    let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+    #endif
+    
+    guard let filePath = filePath else {
+      return
+    }
+    
+    guard let options = FirebaseOptions(contentsOfFile: filePath) else {
+      return
+    }
+    
+    FirebaseApp.configure(options: options)
   }
   
   func applicationDidBecomeActive(_ application: UIApplication) {
