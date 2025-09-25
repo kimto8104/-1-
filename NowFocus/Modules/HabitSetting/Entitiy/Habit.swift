975e5e3e1cfee5e32f72a278d9b229cb58b7465d
may810 <41029872+kimto8104@users.noreply.github.com>
2025-09-25 21:23:42 +0900

    CategoryをHabitに変更

--- NowFocus/Modules/HabitSetting/Entitiy/Habit.swift
+++ NowFocus/Modules/HabitSetting/Entitiy/Habit.swift
@@ -1,5 +1,5 @@
 //
-//  Category.swift
+//  Habit.swift
 //  FaceDownFocusTimer
 //
 //  Created by iosDevelopers on 2025/09/24.
@@ -8,17 +8,19 @@
 import Foundation
 import SwiftData
 
+// @Modelマクロを使うことでSwiftDataがデータのモデルとして扱ってくれて、様々なことをしてくれる
 @Model
-class Category {
+class Habit {
+    // @AttributeはSwiftDataの属性オプションで、同じ値が重複しない、ユニークなプロパティにしてくれる
     // データID
     @Attribute(.unique) var id: UUID = UUID()
-    // カテゴリー名
-    var categoryName: String
+    // Habit名も重複禁止にする
+    @Attribute(.unique) var habitName: String
     // 習慣化させる理由
     var reason: String?
     
-    init (categoryName: String, reason: String? = nil) {
-        self.categoryName = categoryName
+    init (habitName: String, reason: String? = nil) {
+        self.habitName = habitName
         self.reason = reason
     }
 }
--- NowFocus/Utilities/ModelContainerManager.swift
+++ NowFocus/Utilities/ModelContainerManager.swift
@@ -17,58 +17,67 @@ import Foundation
 
 @MainActor
 class ModelContainerManager {
-  static let shared = ModelContainerManager()
-  let container: ModelContainer?
-  private init() {
-    do {
-      container = try ModelContainer(for: FocusHistory.self)
-    } catch {
-      print("Failed to save SwiftData at \(#line) Fix It")
-      container = nil
+    static let shared = ModelContainerManager()
+    let container: ModelContainer?
+    private init() {
+        do {
+            container = try ModelContainer(for: FocusHistory.self)
+        } catch {
+            print("Failed to save SwiftData at \(#line) Fix It")
+            container = nil
+        }
     }
-  }
-  
-  @MainActor func saveFocusHistory(history: FocusHistory) {
-    print("ModelContainerManager: 保存開始 - カテゴリー: \(history.category ?? "nil"), 時間: \(history.duration), 開始日時: \(history.startDate)")
-    self.container?.mainContext.insert(history)
-    do {
-        try self.container?.mainContext.save()
-        print("ModelContainerManager: 保存成功 - カテゴリー: \(history.category ?? "nil"), 時間: \(history.duration), 開始日時: \(history.startDate)")
-    } catch {
-        print("ModelContainerManager: 保存失敗 - \(error)")
+    
+    @MainActor func saveFocusHistory(history: FocusHistory) {
+        self.container?.mainContext.insert(history)
+        do {
+            try self.container?.mainContext.save()
+        } catch {
+            print("ModelContainerManager: 保存失敗 - \(error)")
+        }
     }
-  }
-  
-  @MainActor func removeCategoryFromHistory(category: String) {
-    print("ModelContainerManager: カテゴリー削除開始 - \(category)")
     
-    // UserDefaultsからカテゴリーを削除
-    var savedCategories = UserDefaultManager.savedCategories
-    // カテゴリーが一致すればUserDefaultから削除
-    savedCategories.removeAll { $0 == category }
-    // UserDefaultのカテゴリーを更新
-    UserDefaultManager.savedCategories = savedCategories
-    
-    // SwiftDataの該当カテゴリーの履歴を検索
-    let descriptor = FetchDescriptor<FocusHistory>(
-      predicate: #Predicate<FocusHistory> { history in
-        history.category == category
-      }
-    )
-    
-    do {
-      let histories = try container?.mainContext.fetch(descriptor) ?? []
-      print("ModelContainerManager: 対象履歴数 - \(histories.count)")
-      
-      // 該当する履歴を完全に削除
-      for history in histories {
-        container?.mainContext.delete(history)
-      }
-      // SwiftData保存
-      try container?.mainContext.save()
-      print("ModelContainerManager: カテゴリー削除完了")
-    } catch {
-      print("ModelContainerManager: カテゴリー削除失敗 - \(error)")
+    // カテゴリーを保存
+    @MainActor func saveHabit(habit: Habit) {
+        print("Habit保存開始：\(habit)")
+        self.container?.mainContext.insert(habit)
+        do {
+            try self.container?.mainContext.save()
+        } catch {
+            print("Failed to save Habit")
+        }
     }
-  }
+    
+    @MainActor func removeCategoryFromHistory(category: String) {
+        print("ModelContainerManager: カテゴリー削除開始 - \(category)")
+        
+        // UserDefaultsからカテゴリーを削除
+        var savedCategories = UserDefaultManager.savedCategories
+        // カテゴリーが一致すればUserDefaultから削除
+        savedCategories.removeAll { $0 == category }
+        // UserDefaultのカテゴリーを更新
+        UserDefaultManager.savedCategories = savedCategories
+        
+        // SwiftDataの該当カテゴリーの履歴を検索
+        let descriptor = FetchDescriptor<FocusHistory>(
+            predicate: #Predicate<FocusHistory> { history in
+                history.category == category
+            }
+        )
+        
+        do {
+            let histories = try container?.mainContext.fetch(descriptor) ?? []
+            print("ModelContainerManager: 対象履歴数 - \(histories.count)")
+            
+            // 該当する履歴を完全に削除
+            for history in histories {
+                container?.mainContext.delete(history)
+            }
+            // SwiftData保存
+            try container?.mainContext.save()
+            print("ModelContainerManager: カテゴリー削除完了")
+        } catch {
+            print("ModelContainerManager: カテゴリー削除失敗 - \(error)")
+        }
+    }
 }
